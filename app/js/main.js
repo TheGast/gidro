84af04859c7059222f1e7d917eceaef2f1956c0f
TheGast <wotthegast@yandex.ru>
2020-10-06 17:19:00 +0300

    added banner-section

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,14 @@
 $(function(){
 
 
+    $('.banner-section__slider').slick({
+
+      autoplay: true,
+      autoplaySpeed: 3000,
+      dots: true,
+      prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/arrow-left.svg" alt=""></button>',
+      nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg" alt=""></button>',
+    });
+
 	
 });
