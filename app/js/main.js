814cd48cca6ae069d9fc4598bb18b4f77975f912
TheGast <wotthegast@yandex.ru>
2020-10-10 23:37:48 +0300

    end adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -20,12 +20,45 @@ $(function(){
 
     $('.product-slider').slick({
 
-      autoplay: false,
+      autoplay: true,
       autoplaySpeed: 3000,      
       slidesToShow: 4,
       slidesToScroll: 1,      
       prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/arrow-black-left.svg" alt=""></button>',
       nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/arrow-black-right.svg" alt=""></button>',
+      responsive: [
+        {
+          breakpoint: 1301,
+          settings: {
+            arrows: false,  
+            dots: true,          
+          }
+        },        
+        {
+          breakpoint: 1201,
+          settings: {
+            arrows: false,  
+            dots: true,   
+            slidesToShow: 3,       
+          }
+        },        
+        {
+          breakpoint: 870,
+          settings: {            
+            slidesToShow: 2,
+            dots: true, 
+            arrows: false,           
+          }
+        },        
+        {
+          breakpoint: 590,
+          settings: {            
+            slidesToShow: 1,  
+            dots: true,  
+            arrows: false,   
+          }
+        },        
+      ]
     });
 
 
@@ -34,10 +67,11 @@ $(function(){
         e.preventDefault();
 
         $($(this).siblings()).removeClass('tab--active');
-        $($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
+        $($(this).parent().parent().siblings().find('div')).removeClass('tabs-content--active');
 
         $(this).addClass('tab--active');
-        $($(this).attr('href')).addClass('tabs-content--active');        
+        $($(this).attr('href')).addClass('tabs-content--active');  
+        $('.product-slider').slick('setPosition');      
     });
 
 
@@ -74,6 +108,16 @@ $(function(){
 
     $('.menu__btn').on('click', function(){
       $('.menu-mobile__list').toggleClass('menu-mobile__list--active');
+    });
+
+    $('.footer__top-title').on('click', function(){
+      $(this).next().slideToggle();
+      $(this).toggleClass('footer__topdrop--active');      
+    });
+
+    
+    $('.aside__btn').on('click', function(){
+      $(this).next().slideToggle(); 
     });
 	
 });
